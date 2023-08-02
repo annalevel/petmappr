b748e62458a703493265e244c22b145bc1e5a00d
Anna Level <anna.e.m.level@gmail.com>
2023-08-01 20:36:12 -0500

    Move .env

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,6 @@
 import { generateToken } from './middleware/authentication'
 import express from 'express';
 import { attachRoutes } from './routes/routes';
-require('dotenv').config({ path: __dirname + '/../.env'});
 
 const app = express();
 const port = process.env.PORT;
--- api/routes/routes.ts
+++ api/routes/routes.ts
@@ -1,5 +1,6 @@
 import { Application } from 'express';
 import * as pets from '../controllers/pets';
+require('dotenv').config();
 
 export const attachRoutes = (app: Application): void => {
     app.get('/api/v1/pets', pets.getPets);
