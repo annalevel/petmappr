32c51fce342ecd050f523ec4c06de498a30562dd
Anna Level <anna.e.m.level@gmail.com>
2023-08-06 12:53:18 -0500

    Fix CORS issue

--- api/index.ts
+++ api/index.ts
@@ -5,6 +5,12 @@ import { attachRoutes } from './routes/routes';
 const app = express();
 const port = process.env.PORT;
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 generateToken();
 
 attachRoutes(app);
