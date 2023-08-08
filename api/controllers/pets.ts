a204dee1d5e3e6401ee6229695d96a7f99bcc373
Anna Level <anna.e.m.level@gmail.com>
2023-08-08 12:21:27 -0500

    Pass parameters to external API

--- api/controllers/pets.ts
+++ api/controllers/pets.ts
@@ -1,18 +1,31 @@
 import axios from '../middleware/authentication';
 import { Request, Response } from 'express';
 
+/*
+Get pets matching a specified species, gender, and location
+*/
 export const getPets = async(req: Request, res: Response) => {
     try {
+        const userParameters = req.query;
+        const sanitizedParameters: any = {};
+
+        if (userParameters.species == "dog" || userParameters.species == "cat") {
+          sanitizedParameters.type = userParameters.species;
+        }
+        if (userParameters.gender == "male" || userParameters.gender == "female") {
+          sanitizedParameters.gender = userParameters.gender;
+        }
+        if (userParameters.location !== null && userParameters.location !== undefined) {
+          sanitizedParameters.location = userParameters.location;
+        }
+
         const { data } = await axios.get(`${process.env.PETFINDER_API_BASE_URL}/animals`, {
-          // params: {
-          //   location: 'your_location',
-          //   type: 'dog', // or 'cat' for cats
-          // },
+          params: sanitizedParameters
         });
     
         res.json(data);
       } catch (error) {
         console.error(error);
-        res.status(500).json({ error: 'Internal Server Error' });
+        res.status(500).json({ error: 'An error has occurred.' });
       }
     };
